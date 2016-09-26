ef95c4da7066b7236147a4eaaddd562e21e725b0
Ijat <ijat@users.noreply.github.com>
2016-09-27 01:32:35 +0800

    Update ftpbak.sh

--- ftpbak.sh
+++ ftpbak.sh
@@ -12,10 +12,12 @@ LOG_PATH=/var/log
 LOG_SCRIPT=ftpbak.script.log
 LOG_RUN=ftpbak.run.log
 
+echo -e "Script run at $THIS_DATE" >> $LOG_PATH/$LOG_SCRIPT 2>&1
+
 lftp -c "set ftp:list-options -a;
 open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST;
 lcd $LOCAL_PATH;
 cd $FTP_PATH;
 mirror --reverse --delete --use-cache --verbose --allow-chown --allow-suid --no-umask --parallel=2" | tee $LOG_PATH/$LOG_RUN
 
-echo -e "Script run at $THIS_DATE" >> $LOG_PATH/$LOG_SCRIPT 2>&1
+
